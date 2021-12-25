2d459566e211f8394892554807a94c591e356861
Muhammadrazabangi09 <muhammad.raza@cooperativecomputing.com>
2021-12-25 22:28:13 +0500

    connect with firebase database

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+const firebaseConfig = {
+apiKey: "AIzaSyC2WAdkqFwslc52I_f2eqoL85Gc2j6SF3s",
+authDomain: "raza-todo.firebaseapp.com",
+databaseURL: "https://raza-todo-default-rtdb.firebaseio.com",
+projectId: "raza-todo",
+storageBucket: "raza-todo.appspot.com",
+messagingSenderId: "921254429095",
+appId: "1:921254429095:web:425ed377113f50cde47682",
+measurementId: "G-R4X8B6X64D"
+};
+const app = firebase.initializeApp(firebaseConfig);
+let database = app.database();
+
 let todoItem = "todoItem";
 let showList = "showList";
 let errorShow = "error";
@@ -12,33 +25,19 @@ function create(){
     let error = getId(errorShow);
 
     if (item.value != "") {
-
         if (item.value.length <= 3) {
             error.setAttribute("class","error");
             error.innerHTML = `Data must be greater than 3 characters.`;
         } else {
             error.innerHTML = ``;
-            let todoList = getId(showList);
             clearAllBtn.classList.remove('d-none');
-            clearAllBtn.classList.add('d-block');
-            let li = document.createElement("li");
-            let span = document.createElement("span");
-            let button = document.createElement("button");
-            let i = document.createElement("i");
-            li.appendChild(span);
-            li.appendChild(button);
-            button.appendChild(i);
-            span.setAttribute("contentEditable",true);
-            span.setAttribute("onblur","edit(this)");
-            span.setAttribute("onclick","enter(this)");
-            button.setAttribute("contentEditable",false);
-            li.setAttribute("onkeypress","return (this.innerText.length < 30)");
-            li.setAttribute("onkeyup","return (this.innerText.length < 30)");
-            button.setAttribute("onclick","deleteItem(this)");
-            i.setAttribute("class","fas fa-trash-alt");
-            todoList.appendChild(li);
-            span.innerHTML = item.value;
-
+            clearAllBtn.classList.add('d-block');            
+            var key = database.ref("/").push().key
+            var todoObj = {
+                key : key,
+                todo : item.value
+            }
+            database.ref("todoApp").child(key).set(todoObj)
             item.value = "";
         }
         
@@ -48,16 +47,49 @@ function create(){
     }
 }
 
+database.ref("todoApp").on("child_added" , function(data){
+    if (data.val().todo != "") {
+        let clearAllBtn = getId('clearAll');
+        clearAllBtn.classList.remove('d-none');
+        clearAllBtn.classList.add('d-block');  
+    }
+
+    let todoList = getId(showList);
+    let li = document.createElement("li");
+    let span = document.createElement("span");
+    let button = document.createElement("button");
+    let i = document.createElement("i");
+    li.appendChild(span);
+    li.appendChild(button);
+    button.appendChild(i);
+    span.setAttribute("contentEditable",true);
+    span.setAttribute("id" , data.val().key);
+    span.setAttribute("onblur","edit(this)");
+    span.setAttribute("onclick","enter(this)");
+    button.setAttribute("contentEditable",false);
+    li.setAttribute("onkeypress","return (this.innerText.length < 30)");
+    li.setAttribute("onkeyup","return (this.innerText.length < 30)");
+    button.setAttribute("onclick","deleteItem(this)");
+    button.setAttribute("id" , data.val().key);
+    i.setAttribute("class","fas fa-trash-alt");
+    todoList.appendChild(li);
+    span.innerHTML = data.val().todo;
+});
+
 function clearAll(e){
     let list = getId(showList);
     error.innerHTML = ``;
     list.innerHTML = "";
+    database.ref("/todoApp").remove();
     e.classList.remove('d-block');
     e.classList.add('d-none');
 }
 
 function edit(e){
     e.innerHTML = e.innerHTML;
+    database.ref("todoApp").child(e.id).update({
+        todo : e.innerHTML
+    })
 }
 
 function deleteItem(e){
@@ -67,6 +99,7 @@ function deleteItem(e){
     e.parentNode.style.opacity = 0;
     setTimeout(function() {
         e.parentNode.remove(e);
+        database.ref("todoApp").child(e.id).remove();
     }, 500);
 }
 
